26dcdf056e79e29d7e30ed97ef41722047cc46dc
Victor Anyadioha <victoranyadioha@gmail.com>
2023-02-21 16:10:38 +0100

    Chore: Code Maintainance and cleanup

--- src/components/PostgreSql/ImportForm.tsx
+++ src/components/PostgreSql/ImportForm.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
-import { databaseApi } from '@/lib/psql-api'
+import { databaseApi } from '@/lib/api'
 import toast, { Toaster } from 'react-hot-toast'
 
 const schema = z.object({
--- src/containers/PostgreSql/PSqlView.tsx
+++ src/containers/PostgreSql/PSqlView.tsx
@@ -1,26 +1,19 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
-import { useState } from 'react'
 
-const [Navbar, DropDown, ImportForm, ExportForm, Footer] = [
+const [Navbar, ImportForm, ExportForm, Footer] = [
   dynamic(() => import('../Layout/Navbar')),
-  dynamic(() => import('@/reuseables/dropdown/DropDown')),
   dynamic(() => import('@components/PostgreSql/ImportForm')),
   dynamic(() => import('@components/PostgreSql/ExportForm')),
   dynamic(() => import('@containers/Layout/Footer')),
 ]
 
 export default function PSqlView() {
-  const [formState, setFormState] = useState<string>('Import')
   return (
     <div>
       <Navbar />
-      <DropDown formState={formState} setFormState={setFormState} />
-      {formState === 'Import' ? (
-        <ImportForm />
-      ) : formState === 'Export' ? (
-        <ExportForm />
-      ) : null}
+      <ImportForm />
+      <ExportForm />
       <Footer />
     </div>
   )
