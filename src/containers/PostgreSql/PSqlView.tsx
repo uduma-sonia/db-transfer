4e241c636f4b4022db7f5ae3a6865d4f4ebfc7cd
Victor Anyadioha <victoranyadioha@gmail.com>
2023-02-21 07:57:11 +0100

    Feat: Hide-and-Reveal form functionality done

--- src/containers/PostgreSql/PSqlView.tsx
+++ src/containers/PostgreSql/PSqlView.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
+import { useState } from 'react'
 
-const [Navbar, ImportForm, ExportForm, Footer] = [
+const [Navbar, DropDown, ImportForm, ExportForm, Footer] = [
   dynamic(() => import('../Layout/Navbar')),
+  dynamic(() => import('@components/PostgreSql/DropDown')),
   dynamic(() => import('@components/PostgreSql/ImportForm')),
   dynamic(() => import('@components/PostgreSql/ExportForm')),
   dynamic(() => import('@containers/Layout/Footer')),
 ]
 
-export default function MySqlView() {
+export default function PSqlView() {
+  const [formState, setFormState] = useState<string>('Import')
   return (
     <div>
       <Navbar />
-      <ImportForm />
-      <ExportForm />
+      <DropDown formState={formState} setFormState={setFormState} />
+      {formState === 'Import' ? (
+        <ImportForm />
+      ) : formState === 'Export' ? (
+        <ExportForm />
+      ) : null}
       <Footer />
     </div>
   )
